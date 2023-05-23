afb3d21ab1506d99413bc7f525bbabf6ad8116e8
Abdul-Samad99 <abdulsamad.ismail@systemsltd.com>
2023-05-23 18:58:51 +0500

    user logged in

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,56 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import Footer from './components/Layout/Footer';
 import SignUp from './components/Forms/SignUp';
 import SignIn from './components/Forms/SignIn';
+import { useState } from 'react';
 
 
 function App() {
+
+  const [users, setUsers] = useState(
+    [{
+      name: "Abdul Samad",
+      email: "samadmemon991@gmail.com",
+      password: "asd",
+      failedAttempts: 0
+    }]
+  );
+  const [signIn, setSignIn] = useState(false);
+  const [signUp, setSignUp] = useState(true);
+
+
+  const getUserSignIn = () => {
+    setSignIn(true);
+    setSignUp(false);
+  }
+  const getUserSignUp = () => {
+    setSignIn(false);
+    setSignUp(true);
+  }
+  const registerUser = (userData) => {
+    setUsers((prevUsers) => {
+      return [...prevUsers, userData]
+    })
+    getUserSignIn();
+  }
+
+
+  const failedAttempt = (failedUser) => {
+    setUsers((prevUsers) => {
+      return prevUsers.map(user => {
+        if (user.email === failedUser.email) {
+          return failedUser;
+        }
+        return user;
+      })
+    })
+    console.log(users);
+  }
+
   return (
     < div className="App">
       <Header />
-      <SignIn />
-      {/* <SignUp /> */}
+      {signIn && <SignIn users={users} failedAttempt={failedAttempt} />}
+      {signUp && <SignUp registerUser={registerUser} />}
       <Footer />
 
     </div>
--- src/components/Forms/SignIn.jsx
+++ src/components/Forms/SignIn.jsx
@@ -1,24 +1,67 @@
-import { FloatingLabel, Form } from "react-bootstrap";
+import { Button, FloatingLabel, Form, Stack } from "react-bootstrap";
 import Card from "../Layout/Card";
+import { useState } from "react";
 
-export default function SignIn() {
+export default function SignIn(props) {
+
+  const [successMsg, setSuccessMsg] = useState(false);
+  const [errorMsg, setErrorMsg] = useState(false);
+  const [blockMsg, setBlockMsg] = useState(false);
+
+  const validate = (e) => {
+    e.preventDefault();
+    const user = props.users.filter((user) => {
+      return user.email === e.target.email.value;
+    })[0]
+
+    if (user === null) {
+      failLogin();
+    } else if (user.failedAttempts >= 3) {
+      blockedUser(user.name)
+    } else if (user.password === e.target.password.value) {
+      successLogin()
+    } else {
+      user.failedAttempts++;
+      props.failedAttempt(user)
+    }
+  }
+
+  const successLogin = () => {
+    console.log("success")
+  }
+
+  const failLogin = () => {
+    console.log("fail")
+
+  }
+
+  const blockedUser = (name) => {
+    console.log(name + " user blocked")
+
+  }
+
+
+
   return (
-    <Card title={"Welcome back"}>
-      <Form className="text-dark">
-        <Form.Group>
-          <FloatingLabel
-            controlId="floatingInput"
-            label="Email address"
-            className="mb-3"
-          >
-            <Form.Control type="email" placeholder="name@example.com" />
-          </FloatingLabel>
-        </Form.Group>
-        <Form.Group>
-          <FloatingLabel controlId="floatingPassword" label="Password">
-            <Form.Control type="password" placeholder="Password" />
-          </FloatingLabel>
-        </Form.Group>
+    <Card title={"Welcome back!"}>
+      <Form className="text-dark" onSubmit={validate}>
+        <Stack>
+          <Form.Group>
+            <FloatingLabel
+              controlId="email"
+              label="Email address"
+              className="mb-3"
+            >
+              <Form.Control type="email" placeholder="name@example.com" />
+            </FloatingLabel>
+          </Form.Group>
+          <Form.Group>
+            <FloatingLabel controlId="password" label="Password">
+              <Form.Control type="password" placeholder="Password" />
+            </FloatingLabel>
+          </Form.Group>
+          <Button className="mt-2" type="submit" variant="dark">Sign in</Button>
+        </Stack>
       </Form>
     </Card>
   );
